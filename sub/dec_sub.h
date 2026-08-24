de9d89dac3d6e0615609fe5ba75a2d7d28bcaa90
Alex Feldgendler <alexey.feldgendler@gmail.com>
2026-08-24 19:42:19 +0200

    sub/dec_sub: add sub_get_last_pkt_pts()
    Return the PTS of the most recent packet read from the demuxer, in the
    subtitle timebase. It is only meaningful compared against itself, as a
    measure of how far the decoder has advanced. The next commit uses it to
    wait until a region has actually been decoded.

--- sub/dec_sub.h
+++ sub/dec_sub.h
@@ -68,6 +68,7 @@ struct sd_times sub_get_times(struct dec_sub *sub, double pts);
 struct sub_lines *sub_get_lines(struct dec_sub *sub);
 
 void sub_reset(struct dec_sub *sub);
+double sub_get_last_pkt_pts(struct dec_sub *sub);
 void sub_select(struct dec_sub *sub, bool selected);
 void sub_set_recorder_sink(struct dec_sub *sub, struct mp_recorder_sink *sink);
 void sub_set_play_dir(struct dec_sub *sub, int dir);
